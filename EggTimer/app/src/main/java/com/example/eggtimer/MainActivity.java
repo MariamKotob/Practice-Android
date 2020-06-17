d6231d1ba7268f721ae81fca0572c2842e664db2
Mariam <memoabdo2036@gmail.com>
2020-06-17 07:26:15 +0200

    add functionality to start button and time slider

--- EggTimer/app/src/main/java/com/example/eggtimer/MainActivity.java
+++ EggTimer/app/src/main/java/com/example/eggtimer/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.eggtimer;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
@@ -16,8 +17,64 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    SeekBar timerSeekBar;
+    TextView timerTextView;
+    Button controllerButton;
+    CountDownTimer countDownTimer;
+    Boolean isActive = false;
+
+    //Method to calculate the time value in minutes and seconds
+    public void updateTime( int secondsLeft){
+        int timeInMinutes = (int) secondsLeft/60;
+        int timeInSeconds = secondsLeft - timeInMinutes * 60;
+        timerTextView.setText(Integer.toString(timeInMinutes) + ":" + Integer.toString(timeInSeconds));
+    }
+
+    //Method to reset the activity to the stsrt
+    public void resetTimer(){
+        timerTextView.setText("0:30");
+        timerSeekBar.setProgress(30);
+        countDownTimer.cancel();
+        controllerButton.setText("GO!");
+        timerSeekBar.setEnabled(true);
+        isActive = false;
+    }
+
+    //on click method for the button to start and stop timer
+    public  void  controlTimer(View view){
+
+        if(isActive == false) {
+            isActive = true;
+            timerSeekBar.setEnabled(false);
+            controllerButton.setText("Stop");
+
+            //code to start the timer
+            countDownTimer = new CountDownTimer(timerSeekBar.getProgress() * 1000 + 100, 1000) {
+
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    updateTime((int) millisUntilFinished / 1000);
+                }
+
+                @Override
+                public void onFinish() {
+                    resetTimer();
+                    MediaPlayer mPlayer = MediaPlayer.create(getApplicationContext(), R.raw.airhorn);
+                    mPlayer.start();
+                }
+            }.start();
+
+        }else{
+
+            resetTimer();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +84,32 @@ public class MainActivity extends AppCompatActivity {
        // setSupportActionBar(toolbar);
 
 
+        timerSeekBar = (SeekBar) findViewById(R.id.timerSB);
+        timerTextView = (TextView) findViewById(R.id.timertv);
+        controllerButton = (Button) findViewById(R.id.controllerButton);
+
+        /*
+         * code for the slider and tracking the value of it's movement
+         */
+        timerSeekBar.setMax(600);
+        timerSeekBar.setProgress(30);
+
+        timerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                updateTime(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
         /*
         //Repeating code based on time chunks (timer) CountDownTimer class
